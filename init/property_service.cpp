9e15c4e21144fa7630e95b409b0678cf7a18348f
Park Ju Hyung <qkrwngud825@gmail.com>
2019-12-03 16:09:07 +0900

    init: add several more keys to spoof
    Change-Id: I0025a89eb326dfc5106288406a15baae2a158f79
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -786,7 +786,9 @@ static const char *snet_prop_key[] = {
 	"ro.debuggable",
 	"ro.secure",
 	"ro.build.type",
+	"ro.build.keys",
 	"ro.build.tags",
+	"ro.system.build.tags",
 	"ro.build.selinux",
 	NULL
 };
@@ -802,6 +804,8 @@ static const char *snet_prop_value[] = {
 	"0",
 	"1",
 	"user",
+	"release-keys",
+	"release-keys",
 	"release-keys",
 	"1",
 	NULL
